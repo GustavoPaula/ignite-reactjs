78efb5171443116d58ff1a8057dec9847cc2d97b
GustavoPaula <gustavopaula095@gmail.com>
2023-08-15 22:38:57 -0300

    componente: comment

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/GustavoPaula.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Silva de Paula</strong>
+              <time title="14 de Agosto às 22:40" dateTime="2023-08-14 22:40:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+      </div>
+
+      <footer>
+        <button>
+          <ThumbsUp />
+          Aplaudir <span>20</span>
+        </button>
+      </footer>
+    </div>
+  )
+}
