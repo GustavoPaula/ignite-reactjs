226d63fa1e3d9176e17a48612421e0021452d984
GustavoPaula <gustavopaula095@gmail.com>
2023-08-20 21:05:57 -0300

    comunicacao entre componentes

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -33,6 +33,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentario ${comment}`)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -75,7 +79,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment}/>
+          return (
+            <Comment 
+              key={comment} 
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
